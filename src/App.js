e3ca9065c3dbd97f9305f24bce05a55c7551f09e
hhimanshu <harit.himanshu@yahoo.in>
2020-07-02 08:10:27 -0700

    Creating Custom Service Worker with Create React App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [name, setName] = useState("")
+    const [userInfo, setUserInfo] = useState({})
+    const handleChange = e => setName(e.target.value);
+    const handleClick = () => {
+
+        fetch(`https://api.github.com/users/${name}`)
+            .then((res => res.json()))
+            .then(data => setUserInfo(data))
+    }
+
+    return (
+        <div className="App">
+            <h1>Github User Information</h1>
+            <div className={'form'}>
+                <input type='text' value={name} onChange={handleChange} className={'text-box'}/>
+                <input type='button' onClick={handleClick} value={'Submit'} className={'button'}/>
+            </div>
+
+            {Object.keys(userInfo).length > 0 && <div className={'table'}>
+                <table>
+                    <tbody>
+                    {Object.entries(userInfo).map(([k, v], i) => {
+                        return <tr key={i}>
+                            <td>{k}</td>
+                            <td>{v}</td>
+                        </tr>
+                    })}
+                    </tbody>
+                </table>
+            </div>}
+        </div>
+    );
 }
 
 export default App;
